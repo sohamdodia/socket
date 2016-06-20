c696b12d29cf0094f4bb04b90cbfe6eeed77bba8
Soham Dodia <sohamdodia@gmail.com>
2016-06-20 11:30:53 +0530

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,8 @@ var socket = io();
 socket.on('connect',function () {
 	console.log('User Connected via socket.io');
 });
+
+socket.on('message',function (message) {
+	console.log('New message:');
+	console.log(message.text);
+});
